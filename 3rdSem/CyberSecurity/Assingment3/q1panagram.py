73a13b34d78288b94b2c6c073505dced1bdbe1b2
Naman Gairola <naman.gairola2001@gmail.com>
2020-09-06 17:56:03 +0530

    Create q1panagram.py

---
+++ 3rdSem/CyberSecurity/Assingment3/q1panagram.py
@@ -0,0 +1,12 @@
+import string
+str=input()
+str=str.lower()
+s=list(set(str))
+i=0
+for j in s:
+    if(ord(j)>=97 and ord(j)<=122):
+        i=i+1
+if(i==26):
+    print("It is panagram")
+else:
+    print("It is not panagram")
